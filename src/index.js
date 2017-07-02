7421c8e437c0b121ddac6bcaf59b1d735c502cac
Chris Bruford <chrisbruford@gmail.com>
2017-07-02 22:31:35 +0100

    chore: update build tooling

--- src/index.js
+++ src/index.js
@@ -25,7 +25,7 @@ function createWindow() {
 
     // and load the index.html of the app.
     mainWindow.loadURL(url.format({
-        pathname: path.resolve(__dirname, "../dist/index.html"),
+        pathname: path.resolve(__dirname, "index.html"),
         protocol: 'file',
         slashes: true
     }));
