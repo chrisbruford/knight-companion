73ac22b569dac550e079b11220d1c3801ae3a856
Chris Bruford <chrisbruford@gmail.com>
2017-07-16 13:03:26 +0100

    temp: set dashboard to be initial view for dev

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
-    { path:'', component: LoginComponent },
+    { path:'**', component: DashboardComponent },
+    //{ path:'', component: LoginComponent },
     { path: '**', component: LoginComponent}
 ];
 
