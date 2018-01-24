6493555a016999d32f40e4c40418bcc23d54de9c
Chris Bruford <chrisbruford@gmail.com>
2018-01-24 20:31:28 +0000

    feat: emit user changes as observable

--- src/app/core/services/user.service.ts
+++ src/app/core/services/user.service.ts
@@ -1,13 +1,18 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { User, SimpleUser } from '../../shared/interfaces/user';
-import { Observable } from 'rxjs';
-import { map, catchError } from 'rxjs/operators';
+import { Observable } from 'rxjs/Observable';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import { map, catchError, tap } from 'rxjs/operators';
 
 @Injectable()
 export class UserService {
 
-    user: User;
+    private _user = new BehaviorSubject<any>(null);
+    
+    get user() {
+        return this._user.asObservable();
+    }
 
     constructor(private http: HttpClient) { }
 
@@ -16,7 +21,7 @@ export class UserService {
         return this.http.post<User>(`${process.env.API_ENDPOINT}/login`, data)
             .map(user => {
                 if (user) {
-                    this.user = user;
+                    this._user.next(user);
                     return user;
                 } else {
                     return Observable.throw(new Error("No such user found"));
@@ -30,6 +35,11 @@ export class UserService {
     authCheck(): Observable<User | null> {
         return this.http.get<User | null>(`${process.env.API_ENDPOINT}/authcheck`)
             .pipe(
+            tap(user => {
+                if (user) {
+                    this._user.next(user);
+                }
+            }),
             catchError(err => Observable.throw(err))
             )
     }
@@ -37,6 +47,7 @@ export class UserService {
     logout(): Observable<boolean> {
         return this.http.get<boolean>(`${process.env.API_ENDPOINT}/logout`)
             .pipe(
+            tap(() => this._user.next(null)),
             catchError(err => Observable.throw(err))
             );
     }
