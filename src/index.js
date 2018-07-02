0f47517420323b130286e7da3ebb49e94c3a31d4
Chris Bruford <chrisbruford@gmail.com>
2018-07-02 17:05:42 +0100

    fix: do not do silent install - elevated privliges required

--- src/index.js
+++ src/index.js
@@ -148,7 +148,7 @@ app.on('window-all-closed', function () {
     // On OS X it is common for applications and their menu bar
     // to stay active until the user quits explicitly with Cmd + Q
     if (process.platform !== 'darwin') {
-        app.quit()
+        app.quit();
     }
 });
 
@@ -156,10 +156,10 @@ app.on('activate', function () {
     // On OS X it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
     if (mainWindow === null) {
-        createWindow()
+        createWindow();
     }
 });
 
 function updateApp() {
-    autoUpdater.quitAndInstall(true, true);
+    autoUpdater.quitAndInstall(false, true);
 }
