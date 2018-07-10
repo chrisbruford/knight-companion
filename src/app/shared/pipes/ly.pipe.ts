be8c72f9434eb441f9a7bb2dc3b7ef1efac671a9
Chris Bruford <chrisbruford@gmail.com>
2018-07-10 08:55:01 +0100

    fix: use decimalPipe in lightyear pipe

--- src/app/shared/pipes/ly.pipe.ts
+++ src/app/shared/pipes/ly.pipe.ts
@@ -1,16 +1,23 @@
 import { Pipe } from '@angular/core';
+import { DecimalPipe } from '@angular/common';
 import { PipeTransform } from '@angular/core/src/change_detection/pipe_transform';
 @Pipe({
     name: 'ly'
 })
 export class LightyearPipe implements PipeTransform {
-    transform(value: number) {
-        if (value < 1000) {
-            return `${value}ly`
-        } else if (value > 1000 && value < 1000000) {
-            return `${value / 1000}kly`
-        } else if (value > 1000000) {
-            return `${value / 1000000}Mly`
+
+    constructor(private numberPipe: DecimalPipe) {}
+
+    transform(value: string): string {
+        let distance = parseFloat(value);
+        if (isNaN(distance)) {return 'Unknown'}
+
+        if (distance < 1000) {
+            return `${this.numberPipe.transform(distance, '1.0-0')}ly`
+        } else if (distance >= 1000 && distance < 1000000) {
+            return `${this.numberPipe.transform(distance / 1000, '1.0-2')}kly`
+        } else if (distance >= 1000000) {
+            return `${this.numberPipe.transform(distance / 1000000, '1.0-2')}Mly`
         }
     }
 }
