84776a4692675b8075f02d8c54369a8ee94a31ec
Chris Bruford <chrisbruford@gmail.com>
2018-07-10 08:55:01 +0100

    fix: use decimalPipe in lightyear pipe

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
+import { CommonModule, DecimalPipe } from '@angular/common';
 import { FactionPipe, LightyearPipe } from './pipes';
 import { ToArrayPipe } from './pipes/toArray.pipe';
 
 @NgModule({
-    imports: [],
+    imports: [CommonModule],
     exports: [FactionPipe, LightyearPipe, ToArrayPipe],
     declarations: [FactionPipe, LightyearPipe, ToArrayPipe],
-    providers: [],
+    providers: [DecimalPipe],
 })
 export class SharedModule { }
