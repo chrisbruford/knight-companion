9266c96fd5c0f567aab8be2956fc6b931449e34e
Chris Bruford <chrisbruford@gmail.com>
2018-07-10 08:57:42 +0100

    tests: use fakeAsync to test avg speed calcs accurately

--- src/app/dashboard/exploration/exploration.service.spec.ts
+++ src/app/dashboard/exploration/exploration.service.spec.ts
@@ -1,4 +1,4 @@
-import { TestBed, async } from '@angular/core/testing';
+import { fakeAsync, tick } from '@angular/core/testing';
 import { ExplorationService } from './exploration.service';
 import { JournalService } from '../../journal/journal.service';
 import { EventEmitter } from 'events';
@@ -71,17 +71,33 @@ describe('ExplorationService',()=>{
         expect(jumpsCompleted).toEqual(3);
     });
 
-    it('should calculate average travel speed',()=>{
+    it('should calculate average travel speed',fakeAsync(()=>{
         let fsdJump = new FSDJump();
         let averageSpeed: number;
         fsdJump.JumpDist = 100;
 
         explorationService.averageSpeed.subscribe(_averageSpeed=>averageSpeed=_averageSpeed);
-        
-        mockJournalService.emit(JournalEvents.fsdJump,fsdJump);
-        mockJournalService.emit(JournalEvents.fsdJump,fsdJump);
-        mockJournalService.emit(JournalEvents.fsdJump,fsdJump);
 
-        expect(averageSpeed).toBeGreaterThan(0);
-    });
+        setTimeout(()=>mockJournalService.emit(JournalEvents.fsdJump,fsdJump), 3600e3);
+        
+        tick(3600e3);
+        expect(averageSpeed).toBe(0);
+
+        setTimeout(()=>mockJournalService.emit(JournalEvents.fsdJump,fsdJump), 3600e3);
+        tick(3600e3);
+        expect(averageSpeed).toBe(200/1);
+
+        setTimeout(()=>mockJournalService.emit(JournalEvents.fsdJump,fsdJump), 3600e3);
+        tick(3600e3);
+        expect(averageSpeed).toBe(300/2);
+
+        setTimeout(()=>mockJournalService.emit(JournalEvents.fsdJump,fsdJump), 3600e3);
+        tick(3600e3);
+        expect(averageSpeed).toBe(400/3);
+
+        setTimeout(()=>mockJournalService.emit(JournalEvents.fsdJump,fsdJump), 3600e3);
+        tick(3600e3);
+        expect(averageSpeed).toBe(500/4);
+
+    }));
 })
