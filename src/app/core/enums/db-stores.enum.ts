5663847bbb9fb150f7fe4e2a45bbfd82e96d61f0
Chris Bruford <chrisbruford@gmail.com>
2018-07-27 20:11:46 +0100

    refactor: use enum for db object store names

---
+++ src/app/core/enums/db-stores.enum.ts
@@ -0,0 +1,11 @@
+import { JournalEvents } from "cmdr-journal";
+
+export enum DBStore {
+    missionAccepted = "MissionAccepted",
+    completedJournalFiles = "completedJournalFiles",
+    factions = "factions",
+    currentState = "currentState",
+    ships = "ships",
+    materials = "materials",
+    appSettings = "appSettings"
+}
