8de5021486c469ba4aa8f0da5539f6eb250b5e56
Chris Bruford <chrisbruford@gmail.com>
2018-08-02 18:40:47 +0100

    feat: add broadcast service

---
+++ src/app/core/services/broadcast.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { SettingsService } from '../../dashboard/settings/settings.service';
+import { Observable, throwError } from '../../../../node_modules/rxjs';
+import { AppSetting } from '../enums/app-settings.enum';
+import { HttpClient } from '../../../../node_modules/@angular/common/http';
+import { filter } from '../../../../node_modules/rxjs/operators';
+
+@Injectable({
+    providedIn: 'root'
+}) export class BroadcastService {
+    allowBroadcasts: boolean;
+
+    constructor(
+        public settingService: SettingsService,
+        private http: HttpClient
+    ) {
+        settingService.getSetting<boolean>(AppSetting.broadcasts)
+            .then(allowed => this.allowBroadcasts = allowed);
+
+        settingService.settings
+            .pipe(filter(setting => setting.setting === AppSetting.broadcasts))
+            .subscribe(setting => this.allowBroadcasts = setting.value);
+    }
+
+    broadcast<T>(url: string, message: any): Observable<T> {
+        if (this.allowBroadcasts) {
+            return this.http.post<T>(url, message);
+        } else {
+            return throwError(new Error("Broadcasts not enabled"));
+        }
+    }
+}
