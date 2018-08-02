0df136560406de5f2c0429bc788a14ef6b345c83
Chris Bruford <chrisbruford@gmail.com>
2018-08-02 18:39:44 +0100

    feat: add caching of settings

--- src/app/dashboard/settings/settings.service.spec.ts
+++ src/app/dashboard/settings/settings.service.spec.ts
@@ -1,39 +1,86 @@
-import {SettingsService} from './settings.service'
+import { SettingsService } from './settings.service'
 import { DBService } from '../../core/services/db.service';
 import { DBStore } from '../../core/enums/db-stores.enum';
 import { AppSetting } from '../../core/enums/app-settings.enum';
-describe("SettingsService",()=>{
+describe("SettingsService", () => {
     let settingsService: SettingsService;
     let dbServiceSpy: jasmine.SpyObj<DBService>;
 
-    beforeEach(()=>{
-        dbServiceSpy = jasmine.createSpyObj<DBService>('DBService',['putEntry','getEntry']);
+    beforeEach(() => {
+        dbServiceSpy = jasmine.createSpyObj<DBService>('DBService', ['putEntry', 'getEntry']);
         settingsService = new SettingsService(dbServiceSpy);
     });
 
-    it('should exist',()=>{
+    it('should exist', () => {
         expect(settingsService).toBeDefined();
     });
 
-    it('should pass settings to the DB service for persisting',()=>{
-        settingsService.updateSetting(AppSetting.inaraAPIKey,'123456789abcde');
+    it('should pass settings to the DB service for persisting', () => {
+        dbServiceSpy.putEntry.and.callFake(() => Promise.resolve(true));
+        settingsService.updateSetting(AppSetting.inaraAPIKey, '123456789abcde');
         expect(dbServiceSpy.putEntry)
-        .toHaveBeenCalledWith(
-            DBStore.appSettings, 
-            jasmine.objectContaining({key: AppSetting.inaraAPIKey, value: '123456789abcde'})
-        )
+            .toHaveBeenCalledWith(
+                DBStore.appSettings,
+                jasmine.objectContaining({ key: AppSetting.inaraAPIKey, value: '123456789abcde' })
+            )
     });
 
-    it('should fetch settings from the DB service',(done)=>{
-        dbServiceSpy.getEntry.and.callFake((store: string, entry: string)=>{
+    it('should fetch settings from the DB service', (done) => {
+        dbServiceSpy.getEntry.and.callFake((store: string, entry: string) => {
             return Promise.resolve('123456789abcde');
         });
 
         let setting = settingsService.getSetting(AppSetting.inaraAPIKey);
-        setting.then(setting=>{
+        setting.then(setting => {
             expect(setting).toEqual('123456789abcde');
             done();
         });
-    })
+    });
+
+    it('should return cached settings if available', (done) => {
+        dbServiceSpy.getEntry.and.callFake(() => Promise.resolve(undefined));
+        dbServiceSpy.putEntry.and.callFake(() => Promise.resolve(true));
+
+        settingsService.updateSetting(AppSetting.broadcasts, true)
+            .then(() => {
+                settingsService.getSetting(AppSetting.broadcasts)
+                    .then(setting => {
+                        expect(setting).toEqual(true);
+                        expect(dbServiceSpy.getEntry).not.toHaveBeenCalled();
+                        done();
+                    });
+            })
+    });
+
+    it('should cache a setting if fetched from DB', (done) => {
+        dbServiceSpy.getEntry.and.callFake(() => Promise.resolve(true));
+
+        settingsService.getSetting(AppSetting.broadcasts)
+            .then(setting => {
+                expect(setting).toEqual(true);
+                expect(dbServiceSpy.getEntry).toHaveBeenCalled();
+                dbServiceSpy.getEntry.calls.reset();
+            })
+            .then(()=>settingsService.getSetting(AppSetting.broadcasts))
+            .then(setting=>{
+                expect(setting).toEqual(true);
+                expect(dbServiceSpy.getEntry).not.toHaveBeenCalled();
+                done();
+            })
+    });
+
+    it('should cache a setting when set',(done)=>{
+        dbServiceSpy.putEntry.and.callFake(() => Promise.resolve(true));
+
+        settingsService.updateSetting(AppSetting.broadcasts, true)
+            .then(() => {
+                settingsService.getSetting(AppSetting.broadcasts)
+                    .then(setting => {
+                        expect(setting).toEqual(true);
+                        expect(dbServiceSpy.getEntry).not.toHaveBeenCalled();
+                        done();
+                    });
+            })
+    });
 
 })
--- src/app/dashboard/settings/settings.service.ts
+++ src/app/dashboard/settings/settings.service.ts
@@ -2,11 +2,17 @@ import { Injectable } from "@angular/core";
 import { DBService } from "../../core/services/db.service";
 import { DBStore } from "../../core/enums/db-stores.enum";
 import { AppSetting } from "../../core/enums/app-settings.enum";
-import { Observable, BehaviorSubject } from "../../../../node_modules/rxjs";
+import { Subject } from "../../../../node_modules/rxjs";
 
 @Injectable({
     providedIn: 'root'
 }) export class SettingsService {
+    private setting$ = new Subject<{ setting: AppSetting, value: any }>();
+    get settings() {
+        return this.setting$.asObservable();
+    }
+
+    private _settings = new Map<AppSetting, any>();
 
     constructor(
         private dbService: DBService
@@ -14,9 +20,30 @@ import { Observable, BehaviorSubject } from "../../../../node_modules/rxjs";
 
     updateSetting(setting: AppSetting, value: any): Promise<boolean> {
         return this.dbService.putEntry(DBStore.appSettings, { key: setting, value })
+            .then(
+                result => {
+                    if (result) {
+                        this._settings.set(setting, value);
+                        this.setting$.next({
+                            setting,
+                            value
+                        })
+                    }
+                    return result
+                }
+            )
     }
 
     getSetting<T>(setting: AppSetting): Promise<T> {
-        return this.dbService.getEntry<T>(DBStore.appSettings, setting);
+        const _setting: T = this._settings.get(setting);
+        if (_setting !== undefined) {
+            return Promise.resolve(_setting);
+        } else {
+            return this.dbService.getEntry<T>(DBStore.appSettings, setting)
+                .then(_setting => {
+                    this._settings.set(setting, _setting);
+                    return _setting
+                });
+        }
     }
 }
