ee97086513b03415914c57acdf782451b7f69882
Chris Bruford <chrisbruford@gmail.com>
2018-08-03 16:18:23 +0100

    fix: disable eval

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,13 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
 import { AppModule } from './app/app.module';
 import './material-theme.scss';
-if (process.env.ENV === 'production') {
-  enableProdMode();
+
+window['eval'] = global.eval = function () {
+    throw new Error(`Sorry, this app does not support window.eval().`)
 }
+
+if (process.env.ENV === 'production') {
+    enableProdMode();
+}
+
 platformBrowserDynamic().bootstrapModule(AppModule);
