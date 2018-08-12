8c2623537fd33b4e80ab6851e0434cadb11fd4c7
Chris Bruford <chrisbruford@gmail.com>
2018-08-12 01:24:12 +0100

    chore: remove redundent imports

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -6,17 +6,12 @@ import { JournalModule } from '../journal/journal.module';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { MissionsComponent } from './missions/missions.component';
 import { CombatComponent } from './combat/combat.component';
-import { CombatService } from './combat/combat.service';
 import { SharedModule } from '../shared/shared.module';
-import { MissionService } from './missions/mission.service';
 import { ExplorationComponent } from './exploration/exploration.component';
-import { ExplorationService } from './exploration/exploration.service';
 import { KokMaterialModule } from '../kok-material/kok-material.module';
 import { ShipsComponent } from './ships/ships.component';
-import { ShipsService } from './ships/ships.service';
 import { TrackingFaction } from './tracking-faction.service';
 import { MaterialsComponenet } from './materials/materials.component';
-import { MaterialsService } from './materials/materials.service';
 import { SettingsComponent } from './settings/settings.component';
 
 @NgModule({
