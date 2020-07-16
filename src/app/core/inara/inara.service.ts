18f343093404cb9679f4358c93efe93fd2abc2d5
Chris Bruford <chrisbruford@gmail.com>
2020-07-17 00:58:06 +0100

    fix: inara and discord toggles now disable

--- src/app/core/inara/inara.service.ts
+++ src/app/core/inara/inara.service.ts
@@ -1,7 +1,17 @@
 import { Injectable, OnDestroy } from "@angular/core";
 import { InaraEvent } from "./models/inara-event.model";
 import { HttpClient, HttpErrorResponse } from "@angular/common/http";
-import { catchError, retryWhen, delay, take, tap, map, flatMap, takeWhile, filter } from "rxjs/operators";
+import {
+  catchError,
+  retryWhen,
+  delay,
+  take,
+  tap,
+  map,
+  flatMap,
+  takeWhile,
+  filter,
+} from "rxjs/operators";
 import { InaraResponse } from "./models/inara-response.model";
 import { Observable, throwError, combineLatest, Observer } from "rxjs";
 import { DBService } from "../services/db.service";
@@ -16,115 +26,139 @@ import { AppErrorService } from "../services/app-error.service";
 import { AppErrorTitle } from "../error-bar/app-error-title.enum";
 import { InaraError } from "./inara-error";
 import { InaraErrorCode } from "./inara-error-code";
-
+import { LoggerService } from "../services";
 
 @Injectable({
-    providedIn: 'root'
-}) export class InaraService implements OnDestroy {
-    allowInara = false;
-    alive = true;
-    private _events: InaraEvent[] = [];
+  providedIn: "root",
+})
+export class InaraService implements OnDestroy {
+  allowInara = false;
+  alive = true;
+  private _events: InaraEvent[] = [];
 
-    getEvents() {
-        return [...this._events];
+  getEvents() {
+    return [...this._events];
+  }
+
+  constructor(
+    private http: HttpClient,
+    private journal: JournalService,
+    private settingsService: SettingsService,
+    private appError: AppErrorService,
+    private loggerService: LoggerService
+  ) {
+    this.settingsService
+      .getSetting<{ key: string; value: any }>(AppSetting.inaraBroadcasts)
+      .then((setting) => (this.allowInara = setting ? setting.value : false));
+
+    this.settingsService.settings
+      .pipe(
+        takeWhile(() => this.alive),
+        filter((setting) => setting.setting === AppSetting.inaraBroadcasts)
+      )
+      .subscribe((setting) => (this.allowInara = setting.value));
+
+    this.journal.on(JournalEvents.fsdJump, () => this.sendToInara());
+    this.journal.on(JournalEvents.undocked, () => this.sendToInara());
+    this.journal.on(JournalEvents.resurrect, () => this.sendToInara());
+    this.journal.on("Shutdown", () => this.sendToInara());
+  }
+
+  addEvent(event: InaraEvent) {
+    this._events.push(event);
+  }
+
+  submitEvents(): Observable<InaraResponse> {
+    if (!this._events || this._events.length === 0) {
+      return throwError(
+        new InaraError("There are no events to send", InaraErrorCode.NoEvents)
+      );
     }
 
-    constructor(
-        private http: HttpClient,
-        private journal: JournalService,
-        private settingsService: SettingsService,
-        private appError: AppErrorService
-    ) {
-        this.settingsService.getSetting<boolean>(AppSetting.inaraBroadcasts)
-            .then(allow => this.allowInara = allow);
-
-        this.settingsService.settings.pipe(
-            takeWhile(() => this.alive),
-            filter(setting => setting.setting === AppSetting.inaraBroadcasts)
+    if (this.allowInara) {
+      return combineLatest(
+        this.journal.cmdrName,
+        this.settingsService.getSetting<{ setting: AppSetting; value: string }>(
+          AppSetting.inaraAPIKey
         )
-            .subscribe(setting => this.allowInara = setting.value);
-
-        this.journal.on(JournalEvents.fsdJump, () => this.sendToInara());
-        this.journal.on(JournalEvents.undocked, () => this.sendToInara());
-        this.journal.on(JournalEvents.resurrect, () => this.sendToInara());
-        this.journal.on("Shutdown", () => this.sendToInara());
-    }
-
-    addEvent(event: InaraEvent) {
-        this._events.push(event);
-    }
-
-    submitEvents(): Observable<InaraResponse> {
-        if (!this._events || this._events.length === 0) { return throwError(new InaraError("There are no events to send", InaraErrorCode.NoEvents)) }
-
-        if (this.allowInara) {
-            return combineLatest(
-                this.journal.cmdrName,
-                this.settingsService.getSetting<{ setting: AppSetting, value: string }>(AppSetting.inaraAPIKey)
-            )
-                .pipe(
-                    flatMap(([cmdrName, inaraAPIKey]) => {
-                        const submission: Submission = {
-                            header: {
-                                appName: 'knight-companion',
-                                appVersion: remote.app.getVersion(),
-                                APIkey: inaraAPIKey.value,
-                                commanderName: cmdrName,
-                                isDeveloped: true
-                            },
-                            events: this._events
-                        }
-                        return this.http.post<InaraResponse>(process.env.INARA_API_ENDPOINT, submission)
-                    }),
-                    retryWhen(err => {
-                        return err.pipe(
-                            tap(err => console.log(err.message)),
-                            delay(10000),
-                            take(3)
-                        )
-                    }),
-                    map(res => {
-                        if (res.header.eventStatus !== 200) {
-                            //offset subsequent removals by -n each time an element is removed
-                            let n = 0;
-                            if (res.events) {
-                                for (let [i, event] of res.events.entries()) {
-                                    if (event.eventStatus !== 400) { continue }
-                                    else {
-                                        this._events.splice(i - n++, 1);
-                                    }
-                                }
-                            }
-                            throw res
-                        } else {
-                            this._events = [];
-                            return res
-                        }
-                    })
-                )
-        } else {
-            return throwError(new InaraError("Inara broadcasts not allowed", InaraErrorCode.UserDeniedPermission))
-        }
-    }
-
-    private sendToInara(): void {
-        this.submitEvents()
-            .pipe(take(1))
-            .subscribe(
-                res => {
-                    this.appError.removeError(AppErrorTitle.inaraError);
-                },
-                err => {
-                    if (!(err instanceof InaraError)) {
-                        this.appError.addError(AppErrorTitle.inaraError, new Error("Failed to submit data to Inara"));
-                    } else {
-                        //TODO any handling required for specific InaraError
-                    }
+      ).pipe(
+        flatMap(([cmdrName, inaraAPIKey]) => {
+          const submission: Submission = {
+            header: {
+              appName: "knight-companion",
+              appVersion: remote.app.getVersion(),
+              APIkey: inaraAPIKey.value,
+              commanderName: cmdrName,
+              isDeveloped: true,
+            },
+            events: this._events,
+          };
+          return this.http.post<InaraResponse>(
+            process.env.INARA_API_ENDPOINT,
+            submission
+          );
+        }),
+        retryWhen((err) => {
+          return err.pipe(
+            tap((err) => console.log(err.message)),
+            delay(10000),
+            take(3)
+          );
+        }),
+        map((res) => {
+          if (res.header.eventStatus !== 200) {
+            //offset subsequent removals by -n each time an element is removed
+            let n = 0;
+            if (res.events) {
+              for (let [i, event] of res.events.entries()) {
+                if (event.eventStatus !== 400) {
+                  continue;
+                } else {
+                  this._events.splice(i - n++, 1);
                 }
-            );
+              }
+            }
+            throw res;
+          } else {
+            this._events = [];
+            return res;
+          }
+        })
+      );
+    } else {
+      return throwError(
+        new InaraError(
+          "Inara broadcasts not allowed",
+          InaraErrorCode.UserDeniedPermission
+        )
+      );
     }
+  }
 
-    ngOnDestroy() {
-        this.alive = false;
-    }
+  private sendToInara(): void {
+    this.submitEvents()
+      .pipe(take(1))
+      .subscribe(
+        (res) => {
+          this.appError.removeError(AppErrorTitle.inaraError);
+        },
+        (err) => {
+          if (!(err instanceof InaraError)) {
+            this.appError.addError(
+              AppErrorTitle.inaraError,
+              new Error("Failed to submit data to Inara")
+            );
+          } else {
+            this.loggerService.error({
+              message: err.message,
+              originalError: err,
+            });
+          }
+        }
+      );
+  }
+
+  ngOnDestroy() {
+    this.alive = false;
+  }
 }
--- src/app/core/services/broadcast.service.ts
+++ src/app/core/services/broadcast.service.ts
@@ -1,32 +1,36 @@
-import { Injectable } from '@angular/core';
-import { SettingsService } from '../../dashboard/settings/settings.service';
-import { Observable, throwError } from 'rxjs';
-import { AppSetting } from '../enums/app-settings.enum';
-import { HttpClient } from '@angular/common/http';
-import { filter } from 'rxjs/operators';
+import { Injectable } from "@angular/core";
+import { SettingsService } from "../../dashboard/settings/settings.service";
+import { Observable, throwError } from "rxjs";
+import { AppSetting } from "../enums/app-settings.enum";
+import { HttpClient } from "@angular/common/http";
+import { filter } from "rxjs/operators";
 
 @Injectable({
-    providedIn: 'root'
-}) export class BroadcastService {
-    allowBroadcasts: boolean;
+  providedIn: "root",
+})
+export class BroadcastService {
+  allowBroadcasts: boolean;
 
-    constructor(
-        public settingService: SettingsService,
-        private http: HttpClient
-    ) {
-        settingService.getSetting<boolean>(AppSetting.broadcasts)
-            .then(allowed => this.allowBroadcasts = allowed);
+  constructor(
+    public settingService: SettingsService,
+    private http: HttpClient
+  ) {
+    settingService
+      .getSetting<{ key: string; value: any }>(AppSetting.broadcasts)
+      .then(
+        (setting) => (this.allowBroadcasts = setting ? setting.value : true)
+      );
 
-        settingService.settings
-            .pipe(filter(setting => setting.setting === AppSetting.broadcasts))
-            .subscribe(setting => this.allowBroadcasts = setting.value);
+    settingService.settings
+      .pipe(filter((setting) => setting.setting === AppSetting.broadcasts))
+      .subscribe((setting) => (this.allowBroadcasts = setting.value));
+  }
+
+  broadcast<T>(url: string, message: any): Observable<T> {
+    if (this.allowBroadcasts || this.allowBroadcasts === undefined) {
+      return this.http.post<T>(url, message);
+    } else {
+      return throwError(new Error("Broadcasts not enabled"));
     }
-
-    broadcast<T>(url: string, message: any): Observable<T> {
-        if (this.allowBroadcasts || this.allowBroadcasts === undefined) {
-            return this.http.post<T>(url, message);
-        } else {
-            return throwError(new Error("Broadcasts not enabled"));
-        }
-    }
+  }
 }
